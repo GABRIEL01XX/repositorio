32c62fce92227eec49174a756e06ec564f8f082e
GABRIEL01XX <bielzinho.limas2xxx@gmail.com>
2024-10-24 11:41:42 -0300

    Add files via upload

--- common.js
+++ common.js
@@ -8,4 +8,11 @@ const getCSS = (variavel) => {
   size: 16,
   family: getCSS('--font')
     }
-    export {getCSS, tickConfig }
+    function criarGrafico(data, layout) {
+      const grafico = document.createElement('div');
+      grafico.className = 'grafico';
+      document.getElementById('graficos-container').appendChild(grafico);
+      Plotly.newPlot(grafico, data, layout)
+  }
+  
+  export { getCSS, tickConfig, criarGrafico }
